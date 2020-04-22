9700ace47dc2922bd5ed80be4f8af877720480a5
Logan Moseley <git@loganmoseley.com>
2020-04-22 10:49:09 -0400

    Parse the two necessary args
    Only expecting to parse colors atm

--- Sources/CrosswordTokenGenerator/main.swift
+++ Sources/CrosswordTokenGenerator/main.swift
@@ -1,3 +1,23 @@
+import ArgumentParser
 import Foundation
 
-print("Hello, World!")
+struct CrosswordTokenGenerator: ParsableCommand {
+
+    @Argument(help: "The target platform.")
+    var target: Target
+
+    @Argument(help: "The path to a spreadsheet CSV export.")
+    var csvPath: String
+
+    func run() throws {
+        let url = URL(fileURLWithPath: csvPath)
+        let csv = try String(contentsOf: url)
+        print(csv)
+    }
+}
+
+enum Target: String, ExpressibleByArgument {
+    case android, ios
+}
+
+CrosswordTokenGenerator.main()
